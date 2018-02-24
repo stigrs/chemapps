dbbed93463b809ce1f990dc6f3f81db24a9d182a
Sellevåg <Stig-Rune.Sellevag@ffi.no>
2018-02-24 11:49:43 +0100

    Add biased random walk method

--- chem/collision.cpp
+++ chem/collision.cpp
@@ -123,6 +123,88 @@ Collision::Collision(std::istream& from, const std::string& key)
     }
 }
 
+void Collision::biased_random_walk(std::ostream& to) const
+{
+    srs::Format<char> line;
+    line.width(35).fill('-');
+
+    to << "Lennard-Jones collision parameters:\n" << line('-') << '\n';
+
+    to << "Collision model:\t\t";
+    switch (coll_model) {
+    case brw84:
+        to << "BRW 84\n";
+        break;
+    case brw90a:
+        to << "BRW 90a\n";
+        break;
+    case brw90b:
+        to << "BRW 90b\n";
+        break;
+    }
+
+    to << "Collision integral:\t\t";
+    switch (coll_integral) {
+    case troe:
+        to << "Troe\n";
+        break;
+    case forst:
+        to << "Forst\n";
+        break;
+    }
+
+    to << "Collision diameter bath gas:\t" << sigma_bath << " angstrom\n"
+       << "Collision diameter molecule:\t" << sigma_mol << " angstrom\n"
+       << "Collision diameter complex:\t" << sigma_complex() << " angstrom\n"
+       << "Collision well depth bath gas:\t" << epsilon_bath << " K\n"
+       << "Collision well depth molecule:\t" << epsilon_mol << " K\n"
+       << "Collision well depth complex:\t" << epsilon_complex() << " K\n"
+       << "Mass bath gas:\t\t\t" << mass_bath << " amu\n"
+       << "Mass molecule:\t\t\t" << mass_mol << " amu\n"
+       << "Reduced mass complex:\t\t" << reduced_mass() << " amu\n";
+
+    if ((coll_model == brw90a) || (coll_model == brw90b)) {
+        to << "Highest vibrational frequency:\t" << vibr_high << " cm-1\n"
+           << "Average atom/atom mass:\t\t" << average_mass() << " amu\n"
+           << "Local well depth complex:\t" << epsilon_local() << " K\n"
+           << "Local collision diam. complex:\t" << sigma_local()
+           << " angstrom\n";
+    }
+
+    line.width(27).fill('-');
+    to << "Biased random walk results:\n" << line('-') << '\n';
+
+    if (coll_model == brw84) {
+        double e2 = std::sqrt(mean_sqr_energy_transfer_coll());
+        to << "Collision time:\t\t" << time_coll_brw84() << " s\n"
+           << "BRW parameter (s):\t" << s_param_brw84() << " cm-1\n"
+           << "sqrt(<E^2>):\t\t" << e2 << " cm-1\n";
+    }
+    else if ((coll_model == brw90a) || (coll_model == brw90b)) {
+        double om22 = coll_omega22();
+        double d    = dist_interact();
+        double b    = impact_parameter();
+        double etr  = energy_trans_avg();
+        double tc   = time_coll_brw90();
+        double a    = a_decay_param();
+        double c    = c_autocorr_osc_freq();
+        double edot = mean_sqr_int_energy_change();
+        double s    = s_param_brw90();
+        double e2   = std::sqrt(mean_sqr_energy_transfer_coll());
+
+        to << "Collision integral:\t\t" << om22 << '\n'
+           << "Closest interaction distance:\t" << d << " angstrom\n"
+           << "Impact parameter:\t\t" << b << " angstrom\n"
+           << "Average translational energy:\t" << etr << " cm-1\n"
+           << "Collision time:\t\t\t" << tc << " s\n"
+           << "A decay parameter:\t\t" << a << " s-1\n"
+           << "Autocorr. osc. frequency (C):\t" << c << " s-1\n"
+           << "<Edot(i)^2>:\t\t\t" << edot << " cm-2 s-2\n"
+           << "BRW parameter (s):\t\t" << s << " cm-1\n"
+           << "sqrt(<E^2>):\t\t\t" << e2 << " cm-1\n";
+    }
+}
+
 double Collision::average_mass() const
 {
     double mavg = 0.0;  // average mass of atoms in molecule
@@ -183,6 +265,59 @@ double Collision::epsilon_local() const
     return eloc;
 }
 
+double Collision::coll_omega22() const
+{
+    double red_temp = temperature / epsilon_complex();
+
+    double omega22 = 0.0;
+    switch (coll_integral) {
+    case forst:  // eq. A4.10 in Forst (2003)
+        omega22 = 1.16145 / std::pow(red_temp, 0.148774)
+                  + 0.52487 / std::exp(0.7732 * red_temp)
+                  + 2.16178 / std::exp(2.437887 * red_temp);
+        break;
+    case troe:  // eq. 3.2 in Troe (1977)
+        omega22 = 1.0 / (0.697 + 0.5185 * std::log10(red_temp));
+        break;
+    default:  // unknown
+        omega22 = 0.0;
+    }
+    return omega22;
+}
+
+double Collision::time_coll_brw90() const
+{
+    const double ang2m = 1.0e-10;  // angstrom to meters
+    const double dr    = 0.0005;   // step size
+
+    const double d   = dist_interact();
+    const double b   = impact_parameter();
+    const double etr = energy_trans_avg() * datum::icm_to_K;
+    const double eps = epsilon_complex();
+    const double sig = sigma_complex();
+
+    double r    = d;
+    double veff = 0.0;
+    double tc   = 0.0;
+
+    while (veff < etr) {  // integrate eq. 32 in Kim and Gilbert (1990)
+        double sigr6  = std::pow(sig / r, 6.0);
+        double sigr12 = sigr6 * sigr6;
+        veff = 4.0 * eps * (sigr12 - sigr6) + etr * std::pow(b / r, 2.0);
+        if (veff >= etr) {
+            break;
+        }
+        else {
+            tc += 1.0 / std::sqrt(etr - veff);
+            r -= dr;
+        }
+    }
+    return tc *= (std::sqrt(2.0 * reduced_mass() * datum::m_u) * dr * ang2m
+                  / std::sqrt(datum::k));
+}
+
+//------------------------------------------------------------------------------
+
 void Collision::set_sigma_local_values()
 {
     sigma_loc_val.resize(ptable::get_max_atomic_number(), 0.0);
@@ -209,4 +344,80 @@ void Collision::set_epsilon_local_values()
     epsilon_loc_val[ptable::get_atomic_number("Cl")] = 120.0;
     epsilon_loc_val[ptable::get_atomic_number("Br")] = 190.0;
     epsilon_loc_val[ptable::get_atomic_number("I")]  = 230.0;
+}
+
+double Collision::a_decay_param() const
+{
+    const double ang2m = 1.0e-10;        // angstrom to meters
+    const double dr    = 0.005 * ang2m;  // step size
+
+    const double ebar = energy_trans_avg() * datum::icm_to_K * datum::k;
+    const double eps  = epsilon_local() * datum::k;
+    const double sig  = sigma_local() * ang2m;
+    const double d    = std::max(sig, sig * coll_omega22());
+    const double b    = (2.0 / 3.0) * d;
+
+    double r      = d;
+    double sigr6  = 1.0;
+    double sigr12 = 1.0;
+    double veff   = 0.0;
+    double a      = 0.0;
+
+    while (veff < ebar) {  // evaluate eq. 36 in Kim and Gilbert (1990)
+        sigr6  = std::pow(sig / r, 6.0);
+        sigr12 = sigr6 * sigr6;
+        veff   = 4.0 * eps * (sigr12 - sigr6) + ebar * std::pow(b / r, 2.0);
+        if (veff >= ebar) {
+            break;
+        }
+        else {
+            a = (4.0 * eps * (-12.0 * sigr12 + 6.0 * sigr6)  // force
+                 - 2.0 * ebar * std::pow(b / r, 2.0))
+                / r;
+            r -= dr;
+        }
+    }
+    // Evaluate eq. 34b in Kim and Gilbert (1990):
+
+    a = std::abs((4.0 * eps * (-12.0 * sigr12 + 6.0 * sigr6)
+                  - 2.0 * ebar * std::pow(b / r, 2.0))
+                 / r);
+    return a /= std::sqrt(0.5 * ebar * average_mass() * datum::m_u);
+}
+
+double Collision::mean_sqr_int_energy_change() const
+{
+    // Evaluate eq. 38 in Kim and Gilbert (1990):
+
+    const double ang2m = 1.0e-10;  // angstrom to meter
+
+    const double ebar   = energy_trans_avg() * datum::icm_to_K * datum::k;
+    const double eps    = epsilon_local() * datum::k;
+    const double sig    = sigma_local() * ang2m;
+    const double mlight = mol_mass_lightest() * datum::m_u;
+    const double nu     = vibr_high * datum::c_0 * 100.0;
+    const double k      = 4.0 * datum::pi * datum::pi * mlight * nu * nu;
+    const double deltax = std::sqrt(2.0 * ebar / k);
+    const double f      = 6.0 / 5.0;
+    const double x      = sig / f;
+
+    double deltav = 4.0 * eps * (deltax / x)
+                    * (-12.0 * std::pow(f, 12.0) + 6.0 * std::pow(f, 6.0));
+
+    // Evaluate eq. 40 in Kim and Gilbert (1990):
+
+    return std::pow((ebar - std::min(std::abs(deltav), 0.5 * ebar)) * nu, 2.0)
+           / (datum::J_to_icm * datum::J_to_icm);
+}
+
+double Collision::mol_mass_lightest() const
+{
+    double mlight = 1.0e+6;
+    for (std::size_t i = 0; i < mol_formula.size(); ++i) {
+        double mtmp = ptable::get_atomic_mass(mol_formula[i].atom);
+        if (mtmp < mlight) {
+            mlight = mtmp;
+        }
+    }
+    return mlight;
 }
--- chem/collision.h
+++ chem/collision.h
@@ -18,6 +18,8 @@
 #define CHEM_COLLISION_H
 
 #include <chem/mol_formula.h>
+#include <srs/datum.h>
+#include <algorithm>
 #include <cmath>
 #include <iostream>
 #include <stdexcept>
@@ -33,6 +35,12 @@ struct Collision_error : std::runtime_error {
 //
 // Class providing methods for computing collision integrals and Lennard-Jones
 // collision rates.
+//
+// Algorithms:
+//   Forst, W. Unimolecular Reactions; Cambridge University Press, 2003.
+//   Gilbert, R. G., J. Chem. Phys., 1984, vol. 80, pp. 5501-5509.
+//   Kim, K. F.; Gilbert, R. G. J. Chem. Phys., 1990, vol. 92, pp. 1819-1830.
+//   Troe, J. J. Chem. Phys., 1977, vol. 66, pp. 4758-4775.
 //
 class Collision {
 public:
@@ -56,12 +64,58 @@ public:
     // Local Lennard-Jones collision diam. of system (Kim and Gilbert, 1990).
     double sigma_local() const;
 
+    // Calculate Lennard-Jones collision rate (Forst, 2003).
+    double lj_coll_rate() const;
+
+    // Calculate reduced collision integral.
+    double coll_omega22() const;
+
+    // Collision time using eq. 22 of Gilbert (1984).
+    double time_coll_brw84() const;
+
+    // Collision time using eq. 32 of Kim and Gilbert (1990).
+    double time_coll_brw90() const;
+
+    // Biased random walk parameter s using eq. 24 in Gilbert (1984).
+    double s_param_brw84() const;
+
+    // Biased random walk parameter s using eq. 30 in Kim and Gilbert (1990).
+    double s_param_brw90() const;
+
+    // Mean-squared energy transfer per collision (<E^2>).
+    double mean_sqr_energy_transfer_coll() const;
+
+    // Calculate collision energy transfer in highly excited molecules
+    // using the biased random walk model.
+    void biased_random_walk(std::ostream& to) const;
+
 private:
     // Populate database with local Lennard-Jones collision diameter values.
     void set_sigma_local_values();
 
     // Populate database with local Lennard-Jones well depth values.
     void set_epsilon_local_values();
+
+    // Closest interaction distance (d) (Kim and Gilbert, 1990).
+    double dist_interact() const;
+
+    // Impact parameter (b) (Kim and Gilbert, 1990).
+    double impact_parameter() const;
+
+    // Average translational energy (Kim and Gilbert, 1990).
+    double energy_trans_avg() const;
+
+    // A decay parameter (Kim and Gilbert, 1990).
+    double a_decay_param() const;
+
+    // Autocorrelation oscillation frequency (Kim and Gilbert, 1990).
+    double c_autocorr_osc_freq() const;
+
+    // Mean-squared rate of internal energy change (Kim and Gilbert, 1990).
+    double mean_sqr_int_energy_change() const;
+
+    // Find lightest mass of molecule.
+    double mol_mass_lightest() const;
 
     enum Coll_model_t { generic, brw84, brw90a, brw90b };  // collision models
     enum Coll_omega22_t { troe, forst };  // collision integral equations
@@ -100,6 +154,81 @@ inline double Collision::epsilon_complex() const
 inline double Collision::sigma_complex() const
 {
     return 0.5 * (sigma_bath + sigma_mol);
+}
+
+inline double Collision::lj_coll_rate() const
+{
+    double sig = sigma_complex();
+    double mu  = reduced_mass();
+
+    return 4.5713e-12 * sig * sig * std::sqrt(temperature / mu)
+           * coll_omega22();
+}
+
+inline double Collision::time_coll_brw84() const
+{
+    return sigma_complex() * 1.0e-10
+           / std::sqrt((8.0 * datum::k * temperature)
+                       / (datum::pi * reduced_mass() * datum::a_0));
+}
+
+inline double Collision::s_param_brw84() const
+{
+    return (0.1 * coll_energy / static_cast<double>(number_vibr)) * h_factor
+           * std::sqrt(vibr_avg * datum::c_0 * 100.0 * time_coll_brw84());
+}
+
+inline double Collision::s_param_brw90() const
+{
+    double edot = mean_sqr_int_energy_change();
+    double tc   = time_coll_brw90();
+    double a    = a_decay_param();
+    double c    = c_autocorr_osc_freq();
+
+    return std::sqrt(edot * tc * 2.0 * a / (a * a + c * c));
+}
+
+inline double Collision::mean_sqr_energy_transfer_coll() const
+{
+    double s = 0.0;
+    if (coll_model == brw84) {
+        s = s_param_brw84();
+    }
+    else if ((coll_model == brw90a) || (coll_model == brw90b)) {
+        s = s_param_brw90();
+    }
+    return 2.0 * s * s;
+}
+
+//------------------------------------------------------------------------------
+
+inline double Collision::dist_interact() const
+{
+    double d = sigma_complex() * std::sqrt(coll_omega22());
+    return std::max(d, sigma_complex());
+}
+
+inline double Collision::impact_parameter() const
+{
+    return (2.0 / 3.0) * dist_interact();
+}
+
+inline double Collision::energy_trans_avg() const
+{
+    double etr = 0.0;
+    if (coll_model == brw90a) {
+        etr = coll_energy / static_cast<double>(number_vibr);
+    }
+    else if (coll_model == brw90b) {
+        etr = 2.0e-3 * datum::k * temperature * datum::N_A / datum::icm_to_kJ;
+    }
+    return etr;
+}
+
+inline double Collision::c_autocorr_osc_freq() const
+{
+    const double nu = vibr_high * datum::c_0 * 100.0;
+    return 2.0 * datum::pi * nu;
 }
 
 #endif  // CHEM_COLLISION_H
