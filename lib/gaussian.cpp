35143640f5e72c2ae87b2dc8edd951e01f4a011a
Stig Rune Sellevåg <stigrs@outlook.com>
2018-06-30 12:28:17 +0200

    Modified run

--- lib/gaussian.cpp
+++ lib/gaussian.cpp
@@ -21,6 +21,7 @@
 #include <cstdlib>
 #include <fstream>
 #include <gsl/gsl>
+#include <limits>
 #include <map>
 
 void Gaussian::init(std::istream& from, const std::string& key)
@@ -77,43 +78,34 @@ void Gaussian::init(std::istream& from, const std::string& key)
 
 void Gaussian::run(Molecule& mol) const
 {
-    // Create Gaussian input file:
-    write_com(mol);
+    write_com(mol);  // create Gaussian input file
 
-    bool ok = true;
-    // Run Gaussian:
+    bool ok         = true;
     std::string cmd = version + " " + jobname;
     if (std::system(cmd.c_str()) != 0) {
-        ok = false;
-        // throw Gauss_error("running " + version + " failed");
+        ok = false;  // running Gaussian failed
     }
 
-    // Get Gaussian output data:
     std::ifstream logfile;
     srs::fopen(logfile, jobname + ".log");
+    Gauss_data data(logfile, out);  // get output data
 
-    Gauss_data data(logfile, out);
-
-    // Check termination:
-    if (!data.check_termination()) {
+    if (!data.check_termination()) {  // Gaussian did not terminate normally
         ok = false;
-        // throw Gauss_error("Gaussian did not terminate normally");
     }
-
-    // Check geometry convergence:
-    if (!data.check_opt_conv()) {
+    if (!data.check_opt_conv()) {  // stationary point not found
         ok = false;
-        // throw Gauss_error("stationary point not found");
     }
-
     if (ok) {
-        // Update molecular energy:
-        mol.set_elec_energy(data.get_scf_zpe_energy()[0]);
+        mol.set_elec_energy(data.get_scf_zpe_energy()[0]);  // update energy
 
-        // Update Cartesian coordinates:
         Gauss_coord coord;
         data.get_opt_cart_coord(coord);
-        mol.set_xyz(coord.xyz);
+        mol.set_xyz(coord.xyz);  // update Cartesian coordinates
+    }
+    else {  // calculation failed to converge; set energy to infinity:
+        constexpr double emax = std::numeric_limits<double>::max();
+        mol.set_elec_energy(emax);
     }
 }
 
